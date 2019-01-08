8ae7a54db8b766b3894e66073b280155686340db
Saluc00 <lucas.usereau@ynov.com>
2019-01-08 17:19:36 +0100

    Renew directories / generator finish

---
+++ Logiciel/generateur.py
@@ -0,0 +1,11 @@
+import markdown2
+
+def convert(md_input, html_output):
+    input_file = open(md_input, "r")
+    html = markdown2.markdown(input_file.read())
+
+    html_file = open(html_output, 'w')
+    html_file.writelines(html)
+    print(html)
+
+convert('markdown.md', 'page.html')
