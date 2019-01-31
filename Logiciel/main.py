23392d23885c0fe26c8e22dc82f3b5d6693c62fd
Saluc00 <lucas.usereau@ynov.com>
2019-01-31 12:27:51 +0100

    Update 1.2 // Full Robustesse :D

--- Logiciel/main.py
+++ Logiciel/main.py
@@ -3,11 +3,12 @@ import markdown2
 import os
 import re
 
+# Detecte s'il y a la présence d'une URL
 link_patterns=[(re.compile(r'((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+(:[0-9]+)?|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)'),r'\1')]
 
-
-head = 
-
+# Intégration du head dans les variables ci dessous
+head = "<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset='utf-8'/>\n\t<title>"+ "YES ! J'ai REUSSSIII !!!!" +"</title>\n\t<link rel='stylesheet' type='text/css' href='main.css'/>\n</head>\n<body>\n"
+finHead = "</body>\n</html>"
 
 def convert(md_input, html_output):
     # Ouvre le fichier .md
@@ -18,8 +19,14 @@ def convert(md_input, html_output):
         html_file = open(html_output, 'w')
     if '.html' not in html_output:
         html_file = open(f'{html_output}.html', 'w')
-    html_file.writelines(html)
+    # Ecrit le debut du head
+    html_file.write(head)
+    # Ecrit le fichier .md converti
+    html_file.write(html)
+    #Ecrit les denieres balises (</body> et </html>)
+    html_file.write(finHead)
 
+# Element graphique sympa :D
 print('''                                                                                                
            ,,                                                   ,,                              
  .M"""bgd  db   mm                .M"""bgd mm            mm     db                              
@@ -34,6 +41,7 @@ P"Ybmmd" .JMML. `Mbmo`Mbmmd'     P"Ybmmd"  `Mbmo`Moo9^Yo.`Mbmo.JMML.`MbmdMM   `M
 ''')
 print('Bienvenue sur l\'invite de commande du generateur de site statique.\nVeuillez entrer -i [FICHIER .MD] -u [FICHIER HTML] !\n\n')
 parser = argparse.ArgumentParser()
+# Création des commandes du CLI
 parser.add_argument("-i", '--input',help='Inserer le chemin du fichier .md')
 parser.add_argument("-u", '--output',help='Inserer le chemin du fichier .html')
 args = parser.parse_args()
